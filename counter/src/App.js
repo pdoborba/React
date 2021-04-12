6ce758a98f54dfe44d504b1c851fd1ad7faa308e
Pedro Borba <pedro_borba77@hotmail.com>
2021-04-12 11:31:02 -0300

    Projeto de Contador

---
+++ counter/src/App.js
@@ -0,0 +1,37 @@
+import {Component} from 'react'
+import './App.css';
+
+class App extends Component {
+  state = {
+    contador: 0
+  }
+  increment = () =>{
+    this.setState({
+      contador: this.state.contador + 1
+    })
+  }  
+  decrement = () =>{
+    this.setState({
+      contador: this.state.contador - 1
+    })
+  }
+  clear = () => {
+    this.setState({
+      contador: 0
+    })
+  }
+  render(){ 
+    return (
+      <div className="App">
+        <header className="App-header">
+          Contador: {this.state.contador}
+          <button onClick={this.increment}>+</button>
+          <button onClick={this.decrement}>-</button>
+          <button onClick={this.clear}>Clear</button>
+        </header>
+      </div>
+    );
+  } 
+}
+
+export default App;
